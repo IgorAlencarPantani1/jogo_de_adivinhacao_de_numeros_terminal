7f4edcea58f14ee88888db41bc682b19a52639e9
Igor Alencar Pantani <igoralencarpantani23@gmail.com>
2025-08-26 21:47:53 -0300

    Add files via upload

---
+++ jogo_de_adivinhacao_de_numeros_terminal/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        Random aletorio = new Random();
+        int vadilacao = 0;
+        int numero_aletoria = aletorio.nextInt(10);
+        do {
+            System.out.println("Digite o número que estou pensando");
+            int n1 = leitor.nextInt();
+            if (n1==numero_aletoria){
+                vadilacao = 1;
+                System.out.println("Acertou");
+            }else {
+                System.out.println("Erro! Tentou Novamente");
+            }
+        } while (vadilacao!=1);
+        System.out.println("Fim");
+    }
+}
